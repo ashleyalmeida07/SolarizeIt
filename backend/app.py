74a4fd26bd60221c3f191749939f9b9db4d809be
Ashley Almeida <155456903+ashleyalmeida07@users.noreply.github.com>
2025-09-04 21:14:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -541,3 +541,8 @@ def get_subsidy_info(state):
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+PORT = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
